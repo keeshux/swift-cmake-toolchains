968d93187c8f70cccf31be2bed4db1096cee19e8
Davide <keeshux@gmail.com>
2026-06-26 14:53:52 +0200

    Fix Linux typos

--- swiftc-wrapper.sh
+++ swiftc-wrapper.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
+
 # SPDX-FileCopyrightText: 2026 Davide De Rosa
 #
 # SPDX-License-Identifier: MIT
 
-#!/bin/bash
-if [[ $(uname -s) == "Darwin" ]]; then
+if [ "$(uname -s)" = "Darwin" ]; then
     if ! command -v swiftly >/dev/null 2>&1; then
         echo "swiftly must be installed and available in PATH" >&2
         exit 1
@@ -11,7 +12,7 @@ if [[ $(uname -s) == "Darwin" ]]; then
     exec swiftly run swiftc "$@"
 else
     swiftc_path="${HOME}/.local/share/swiftly/bin/swiftc"
-    if [[ ! -x "${swiftc_path}" ]]; then
+    if [ ! -x "${swiftc_path}" ]; then
         echo "swiftc must exist and be executable at ${swiftc_path}" >&2
         exit 1
     fi
